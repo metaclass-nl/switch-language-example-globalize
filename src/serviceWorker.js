0ebe3e05f7350cceef9bc0439e0c2ec3ca92fdf1
henk <info@metaclass.nl>
2019-01-08 16:03:00 +0100

    Adapted to mimic start-react-app result - added serviceWorker.js with empty funcions

---
+++ src/serviceWorker.js
@@ -0,0 +1,18 @@
+// Placeholder to allow copy paste from
+// https://api-platform.com/docs/client-generator/react#generating-a-progressive-web-app
+
+export function register(config) {
+
+}
+
+// function registerValidSW(swUrl, config) {
+//
+// }
+//
+// function checkValidServiceWorker(swUrl, config) {
+//
+// }
+
+export function unregister() {
+
+}
