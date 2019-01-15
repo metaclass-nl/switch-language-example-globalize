04a0def09203762a6aac75076a55025785a60577
henk <info@metaclass.nl>
2019-01-15 15:10:03 +0100

    Globalize demo - extracted App component with lifecycle methods

---
+++ src/App.js
@@ -0,0 +1,132 @@
+import React, { Component } from 'react';
+
+import Globalize from "globalize";
+
+// Formatters
+var numberFormatter = Globalize.numberFormatter({ maximumFractionDigits: 2 });
+var numberCompactFormatter = Globalize.numberFormatter({
+    compact: "short",
+    minimumSignificantDigits: 1,
+    maximumSignificantDigits: 3
+});
+var currencyFormatter = Globalize.currencyFormatter( "USD" );
+var dateFormatter = Globalize.dateFormatter({ datetime: "medium" });
+var dateWithTimeZoneFormatter = Globalize.dateFormatter({
+    datetime: "full",
+    timeZone: "America/Sao_Paulo"
+});
+var _dateToPartsFormatter = Globalize.dateToPartsFormatter({ datetime: "medium" });
+var dateToPartsFormatter = function( value ) {
+    return _dateToPartsFormatter( value, {
+        datetime: "medium"
+    }).map(function( part ) {
+        switch(part.type) {
+            case "month": return "<strong>" + part.value + "</strong>";
+            default: return part.value;
+        }
+    }).reduce(function( memo, value ) {
+        return memo + value;
+    });
+};
+var relativeTimeFormatter = Globalize.relativeTimeFormatter( "second" );
+var unitFormatter = Globalize.unitFormatter( "mile/hour", { form: "short" } );
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.startTime = new Date();
+        this.state = {elapsedTime: 0};
+    }
+
+    componentDidMount() {
+        this.timerId = setInterval(
+            () => this.tick(),
+            1000
+        );
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timerId);
+    }
+
+    tick() {
+        this.setState({elapsedTime: +( ( this.startTime - new Date() ) / 1000 ).toFixed( 0 )});
+    }
+
+    render() {
+        // Messages.
+        const message1 = Globalize.formatMessage( "message-1", {
+        currency: currencyFormatter( 69900 ),
+        date: dateFormatter( new Date() ),
+        number: numberFormatter( 12345.6789 ),
+        relativeTime: relativeTimeFormatter( this.state.elapsedTime ),
+        unit: unitFormatter( 60 )
+    });
+
+        return <div id="demo">
+            <h1>Globalize App example using Webpack (and React)</h1>
+
+            <p id="intro-1">{Globalize.formatMessage( "intro-1" )}</p>
+            <table border="1" style={{marginBottom: "1em"}}>
+                <tbody>
+                <tr>
+                    <td><span id="number-label">{Globalize.formatMessage( "number-label" )}</span></td>
+                    <td>"<span id="number">{numberFormatter( 12345.6789 )}</span>"</td>
+                </tr>
+                <tr>
+                    <td><span id="number-compact-label">{Globalize.formatMessage( "number-compact-label" )}</span></td>
+                    <td>"<span id="number-compact">{numberCompactFormatter( 12345.6789 )}</span>"</td>
+                </tr>
+                <tr>
+                    <td><span id="currency-label">{Globalize.formatMessage( "currency-label" )}</span></td>
+                    <td>"<span id="currency">{currencyFormatter( 69900 )}</span>"</td>
+                </tr>
+                <tr>
+                    <td><span id="date-label">{Globalize.formatMessage( "date-label" )}</span></td>
+                    <td>"<span id="date">{dateFormatter( new Date() )}</span>"</td>
+                </tr>
+                <tr>
+                    <td><span id="date-time-zone-label">{Globalize.formatMessage( "date-time-zone-label" )}</span></td>
+                    <td>"<span id="date-time-zone">{dateWithTimeZoneFormatter( new Date() )}</span>"</td>
+                </tr>
+                <tr>
+                    <td><span id="date-to-parts-label">{Globalize.formatMessage( "date-to-parts-label" )}</span></td>
+                    <td>"<span id="date-to-parts">{dateToPartsFormatter( new Date() )}</span>"</td>
+                </tr>
+                <tr>
+                    <td><span id="relative-time-label">{Globalize.formatMessage( "relative-time-label" )}</span></td>
+                    <td>"<span id="relative-time">{relativeTimeFormatter( this.state.elapsedTime )}</span>"</td>
+                </tr>
+                <tr>
+                    <td><span id="unit-label">{Globalize.formatMessage( "unit-label" )}</span></td>
+                    <td>"<span id="unit">{unitFormatter( 60 )}</span>"</td>
+                </tr>
+                </tbody>
+            </table>
+            <p id="message-1">
+                {message1}
+            </p>
+            <p id="message-2">
+                {Globalize.formatMessage( "message-2", {
+                    count: 3
+                })}
+            </p>
+        </div>;
+    }
+}
+
+/*
+const fetchHeaders = new Headers({"accept": "application/json", 'X-Requested-With': 'XMLHttpRequest'});
+fetch('manifest.json', { headers: fetchHeaders }).then(response => {
+    return response.json();
+})
+    .then(
+        json => {
+//            alert(JSON.stringify(json));
+        });
+
+// require('i18n/de.js');
+// Globalize.setLocale('de');
+*/
+
+export default App;
