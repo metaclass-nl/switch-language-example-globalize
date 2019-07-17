fac32805ea8fc13c9a54d4a04573db966054cb8d
henk <info@metaclass.nl>
2019-07-17 11:08:36 +0200

    - removed babel from configuration

--- webpack-config.js
+++ webpack-config.js
@@ -31,13 +31,6 @@ module.exports = {
 	},
     module: {
         rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader"
-                }
-            },
             { test: /\.css$/, use: ['style-loader', 'css-loader'] },
             // images and fonts
             {
